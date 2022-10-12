a9dd4be9a62419e98175ef022fa849fb2b82cf9c
lubyelcuritiba <lubyel.lopes@gmail.com>
2022-10-12 01:56:28 -0300

    Finished HTML and CSS
    Implemented all elements about css, responsive design, media query, and updated some elements on html

--- index.php
+++ index.php
@@ -29,6 +29,10 @@
             </nav>
             <!--desktop-->
             <nav class="mobile right">
+                <div class="botao-menu-mobile">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </div>
+                <!--botao-menu-mobile-->
                 <ul>
                     <li><a href="">Home</a></li>
                     <li><a href="">Sobre</a></li>
@@ -42,7 +46,8 @@
         <!--center-->
     </header>
     <section class="banner-principal">
-        <div class="overlay"></div><!--overlay-->
+        <div class="overlay"></div>
+        <!--overlay-->
         <div class="center">
             <form>
                 <h2>Qual seu e-mail?</h2>
@@ -51,7 +56,7 @@
             </form>
         </div>
         <!--center-->
-        
+
     </section>
     <!--banner-principal-->
     <section class="descricao-autor">
@@ -72,7 +77,7 @@
             <!--w50-->
             <div class="w50 left">
                 <!--Pegar imagem depois-->
-                <img id="foto" src="images/minhaFoto.jpg" />
+                <img class="right" src="images/minhaFoto.jpg" />
             </div>
             <!--w50-->
             <div class="clear"></div>
@@ -85,30 +90,31 @@
             <h2 class="title">Especialidades</h2>
             <div class="w33 left box-especialidades">
                 <h3><i class="fa fa-css3" aria-hidden="true"></i></h3>
-                <h3>CSS3</h3>
+                <h4>CSS3</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea illo magni nam, laudantium repudiandae nostrum reiciendis blanditiis labore eveniet est dolore architecto consectetur sit eaque vitae nemo ut eligendi culpa.</p>
             </div>
             <!--box-especialidades-->
             <div class="w33 left box-especialidades">
                 <h3><i class="fa fa-html5" aria-hidden="true"></i></h3>
-                <h3>HTML5</h3>
+                <h4>HTML5</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea illo magni nam, laudantium repudiandae nostrum reiciendis blanditiis labore eveniet est dolore architecto consectetur sit eaque vitae nemo ut eligendi culpa.</p>
             </div>
             <!--box-especialidades-->
             <div class="w33 left box-especialidades">
                 <h3><i class="fa fa-jpy" aria-hidden="true"></i></h3>
-                <h3>JavaScript</h3>
+                <h4>JavaScript</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea illo magni nam, laudantium repudiandae nostrum reiciendis blanditiis labore eveniet est dolore architecto consectetur sit eaque vitae nemo ut eligendi culpa.</p>
             </div>
             <!--box-especialidades-->
+            <div class="clear"></div><!--clear float-->
         </div>
         <!--center-->
     </section>
     <!--especialidades-->
     <section class="extras">
         <div class="center">
-            <div class="w50 left">
-                <h2 class="title">Depoimentos</h2>
+            <div class="w50 left depoimentos-container">
+                <h2 class="title">Depoimentos dos nossos clientes</h2>
                 <div class="depoimento-single">
                     <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore omnis recusandae inventore, voluptate perferendis molestiae vitae in ex debitis fuga vel blanditiis distinctio commodi sed unde sint, perspiciatis quae? Iure?</p>
                     <!--depoimento-descricao-->
@@ -132,7 +138,7 @@
                 <!--depoimento-single-->
             </div>
             <!--w50-->
-            <div class="w50 left">
+            <div class="w50 left servicos-container">
                 <h2 class="title">Serviços</h2>
                 <div class="servicos">
                     <ul>
